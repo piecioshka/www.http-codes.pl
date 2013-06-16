f78dbea55f922583345052c2c741a51165db51d0
piecioshka <piecioshka@gmail.com>
2013-06-17 00:44:25 +0200

    new HTTP codes supported

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Server returns: `404 Not Found`:
 
 - 100 Continue
 - 101 Switching Protocols
+- 102 Processing
 - 200 OK
 - 201 Created
 - 202 Accepted
@@ -25,14 +26,18 @@ Server returns: `404 Not Found`:
 - 204 No Content
 - 205 Reset Content
 - 206 Partial Content
+- 207 Multi-Status
+- 208 Already Reported
+- 226 IM Used
 - 300 Multiple Choices
 - 301 Moved Permamently
 - 302 Found
 - 303 See Other
 - 304 Not Modified
 - 305 Use Proxy
-- 306 (Unused)
+- 306 Switch Proxy
 - 307 Temporary Redirect
+- 308 Permanent Redirect
 - 400 Bad Request
 - 401 Unauthorized
 - 402 Payment Required
@@ -51,13 +56,41 @@ Server returns: `404 Not Found`:
 - 415 Unsupported Media Type
 - 416 Requested Range Not Satisfiable
 - 417 Expectation Failed
+- 418 I'm a teapot
+- 420 Enhance Your Calm
+- 422 Unprocessable Entity
+- 423 Locked
+- 424 Failed Dependency
+- 425 Unordered Collection
+- 426 Upgrade Required
+- 428 Precondition Required
+- 429 Too Many Requests
+- 431 Request Header Fields Too Large
+- 444 No Response
+- 449 Retry With
+- 450 Blocked by Windows Parental Controls
+- 451 Unavailable For Legal Reasons
+- 494 Request Header Too Large
+- 495 Cert Error
+- 496 No Cert
+- 497 HTTP to HTTPS
+- 499 Client Closed Request
 - 500 Internal Server Error
 - 501 Not Implemented
 - 502 Bad Gateway
 - 503 Service Unavailable
 - 504 Gateway Timeout
 - 505 HTTP Version Not Supported
+- 506 Variant Also Negotiates
+- 507 Insufficient Storage
+- 508 Loop Detected
+- 509 Bandwidth Limit Exceeded
+- 510 Not Extended
+- 511 Network Authentication Required
+- 598 Network read timeout error
+- 599 Network connect timeout error
     
 ### Documentation
 
-See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+Source: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+See: http://tools.ietf.org/html/rfc2324
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-// see: http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+// source: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// see: http://tools.ietf.org/html/rfc2324
 
 // default response headers
 $code = 200;
@@ -8,12 +9,16 @@ $prefix = "HTTP/1.1 ";
 $header = "200 OK";
 
 if (isset($_REQUEST['code'])) {
-    $code = $_REQUEST['code'];
+    $code = (int) $_REQUEST['code'];
 }
 
 switch ($code) {
+    // 1xx
     case 100: $header = "100 Continue"; break;
     case 101: $header = "101 Switching Protocols"; break;
+    case 102: $header = "102 Processing"; break;
+    
+    // 2xx
     case 200: $header = "200 OK"; break;
     case 201: $header = "201 Created"; break;
     case 202: $header = "202 Accepted"; break;
@@ -21,14 +26,22 @@ switch ($code) {
     case 204: $header = "204 No Content"; break;
     case 205: $header = "205 Reset Content"; break;
     case 206: $header = "206 Partial Content"; break;
+    case 207: $header = "207 Multi-Status"; break;
+    case 208: $header = "208 Already Reported"; break;
+    case 226: $header = "226 IM Used"; break;
+    
+    // 3xx
     case 300: $header = "300 Multiple Choices"; break;
     case 301: $header = "301 Moved Permamently"; break;
     case 302: $header = "302 Found"; break;
     case 303: $header = "303 See Other"; break;
     case 304: $header = "304 Not Modified"; break;
     case 305: $header = "305 Use Proxy"; break;
-    case 306: $header = "306 (Unused)"; break;
+    case 306: $header = "306 Switch Proxy"; break;
     case 307: $header = "307 Temporary Redirect"; break;
+    case 308: $header = "308 Permanent Redirect"; break;
+    
+    // 4xx
     case 400: $header = "400 Bad Request"; break;
     case 401: $header = "401 Unauthorized"; break;
     case 402: $header = "402 Payment Required"; break;
@@ -47,12 +60,41 @@ switch ($code) {
     case 415: $header = "415 Unsupported Media Type"; break;
     case 416: $header = "416 Requested Range Not Satisfiable"; break;
     case 417: $header = "417 Expectation Failed"; break;
+    case 418: $header = "418 I'm a teapot"; break;
+    case 420: $header = "420 Enhance Your Calm"; break;
+    case 422: $header = "422 Unprocessable Entity"; break;
+    case 423: $header = "423 Locked"; break;
+    case 424: $header = "424 Failed Dependency"; break;
+    case 425: $header = "425 Unordered Collection"; break;
+    case 426: $header = "426 Upgrade Required"; break;
+    case 428: $header = "428 Precondition Required"; break;
+    case 429: $header = "429 Too Many Requests"; break;
+    case 431: $header = "431 Request Header Fields Too Large"; break;
+    case 444: $header = "444 No Response"; break;
+    case 449: $header = "449 Retry With"; break;
+    case 450: $header = "450 Blocked by Windows Parental Controls"; break;
+    case 451: $header = "451 Unavailable For Legal Reasons"; break;
+    case 494: $header = "494 Request Header Too Large"; break;
+    case 495: $header = "495 Cert Error"; break;
+    case 496: $header = "496 No Cert"; break;
+    case 497: $header = "497 HTTP to HTTPS"; break;
+    case 499: $header = "499 Client Closed Request"; break;
+    
+    // 5xx
     case 500: $header = "500 Internal Server Error"; break;
     case 501: $header = "501 Not Implemented"; break;
     case 502: $header = "502 Bad Gateway"; break;
     case 503: $header = "503 Service Unavailable"; break;
     case 504: $header = "504 Gateway Timeout"; break;
     case 505: $header = "505 HTTP Version Not Supported"; break;
+    case 506: $header = "506 Variant Also Negotiates"; break;
+    case 507: $header = "507 Insufficient Storage"; break;
+    case 508: $header = "508 Loop Detected"; break;
+    case 509: $header = "509 Bandwidth Limit Exceeded"; break;
+    case 510: $header = "510 Not Extended"; break;
+    case 511: $header = "511 Network Authentication Required"; break;
+    case 598: $header = "598 Network read timeout error"; break;
+    case 599: $header = "599 Network connect timeout error"; break;
 }
 
 header($prefix . $header);
